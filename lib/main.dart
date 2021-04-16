3cb989c92ded819c64729763feba0ae0769b87b3
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-16 08:56:26 +0300

    add AuthorizationPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'pages/authentication_page.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: HomePage(),
+      home: AuthorizationPage(),
     );
   }
 }
