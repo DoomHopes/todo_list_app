a8ed8d5b4230a2f444c797b5a41040017f9c511b
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-17 14:45:48 +0300

    some changed in AuthService

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,19 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_list_app/models/user_model.dart';
 
 class AuthService {
   final FirebaseAuth _fAuth = FirebaseAuth.instance;
 
-  Future signInWithEmailAndPass(String eMail, String pass) async {
+  // ignore: missing_return
+  Future<UserModel> signInWithEmailAndPass(String eMail, String pass) async {
     try {
       UserCredential credential =
           await _fAuth.signInWithEmailAndPassword(email: eMail, password: pass);
       User user = credential.user;
+      return UserModel.fromFirebase(user);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
+        return null;
       } else if (e.code == 'wrong-password') {
         print('Wrong password provided for that user.');
+        return null;
       }
     }
+  }
+
+  // ignore: missing_return
+  Future<UserModel> registerWithEmailAndPass(String eMail, String pass) async {
+    try {
+      UserCredential credential = await _fAuth.createUserWithEmailAndPassword(
+          email: eMail, password: pass);
+      User user = credential.user;
+      return UserModel.fromFirebase(user);
+    } on FirebaseAuthException catch (e) {
+      return null;
+    }
+  }
+
+  Future logOut() async {
+    await _fAuth.signOut();
   }
 }
