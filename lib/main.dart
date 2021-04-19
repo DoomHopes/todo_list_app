e5fdeb8860b5f4e99a14a26f77f76d05d635c6fd
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-19 10:13:57 +0300

    setPreferredOrientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_app/models/user_model.dart';
 import 'package:todo_list_app/services/auth.dart';
@@ -8,7 +9,14 @@ import 'pages/landing_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
