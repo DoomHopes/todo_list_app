606ae29458d636f98fe47de9898e420399d3d465
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-04-19 10:35:06 +0300

    delete drawer and add button in appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_list_app/widgets/drawer_widget.dart';
+import 'package:todo_list_app/services/auth.dart';
 import 'package:todo_list_app/widgets/search_widget.dart';
 import 'package:todo_list_app/widgets/works_list_widget.dart';
 
@@ -12,13 +12,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int sectionIndex = 0;
 
+  AuthService _service = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ToDo'),
+        title: const Text('ToDo'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 10, right: 20, bottom: 10),
+            child: ElevatedButton(
+              child: Icon(Icons.exit_to_app_rounded),
+              onPressed: () {
+                _service.logOut();
+              },
+            ),
+          ),
+        ],
       ),
-      drawer: DrawerWidget(),
       body: sectionIndex == 0 ? WorksListWidget() : SearchWidget(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
